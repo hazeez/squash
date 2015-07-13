0b407a54c068df9679623151e859b7ec24382c57
hazeez <hafizul.azeez@gmail.com>
2015-07-13 11:38:43 +0530

    production database - pre ready

--- src/squashapp/models.py
+++ src/squashapp/models.py
@@ -81,30 +81,44 @@ class ProjectDatabase(models.Model):
     SUBREGIONS = list((x.sub_region_code, x.sub_region_alias)for x in SubRegionDatabase.objects.all())
     SQANAMES = list((x.sqa_user_name, x.sqa_alias_name)for x in SQAContactDatabase.objects.all())
     PRODUCTS = list((x.product_code, x.product_alias)for x in ProductDatabase.objects.all())
+    MANAGEDBY = (('SQA','SQA'),('PM','PM'))
+    METHODS = (('PRLC','PRLC'),('OUM','OUM'),('APM','APM'))
+    SCMTOOLS = (('SVN','SVN'),('VSS','VSS'))
+    BUGTOOLS = (('JIRA','JIRA'),('BUGZ','BUGZ'),('CUST','CUST'))
 
     project_release_name = models.CharField(max_length=30, blank=False, primary_key=True)
     project_uid = models.CharField(max_length=15, blank=False)
+    project_customer_name = models.CharField(max_length=40, blank=True, default="My Bank")
     project_class = models.CharField(max_length=3, blank=False, choices=PROJECT_TYPES, default="IUT")
     project_products = models.CharField(max_length=15, blank=False, choices=PRODUCTS, default="FCUBS")
+    project_base_version = models.CharField(max_length=30, blank=False, default="No base")
+    project_total_effort = models.IntegerField(default=0,blank=True)
+    project_cut_effort = models.IntegerField(default=0,blank=True)
+    project_testing_effort = models.IntegerField(default=0,blank=True)
+    project_cob_effort = models.IntegerField(default=0,blank=True)
     project_status = models.CharField(max_length=9, blank=False, choices=PROJECT_STATUSES, default="ONG")
     project_manager = models.CharField(max_length=40, blank=True)
+    project_backup_manager = models.CharField(max_length=40, blank=True, default="No Manager")
     project_primary_sqa = models.CharField(max_length=30, choices = SQANAMES, default="hafizul.azeez")
     project_secondary_sqa = models.CharField(max_length=30, blank=True, choices = SQANAMES, default="hafizul.azeez")
     project_tertiary_sqa = models.CharField(max_length=30, blank=True, choices = SQANAMES, default="hafizul.azeez")
     project_region_lead = models.CharField(max_length=30, blank=False, choices = SQANAMES, default="hafizul.azeez")
     project_region = models.CharField(max_length = 4, choices=REGIONS, blank = False, default="EMEA")
     project_subregion = models.CharField(max_length = 4, choices=SUBREGIONS, blank = False, default="AFRI")
+    project_methodology = models.CharField(max_length=4, default="PRLC", choices = METHODS)
     project_created_date = models.DateTimeField(auto_now_add=True, auto_now=False)
     project_updated_date = models.DateTimeField(auto_now=True, auto_now_add=False)
     project_start_date = models.DateField(blank=True,help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
     project_end_date = models.DateField(blank=True, help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
-    project_release_status = models.CharField(max_length=10, blank=False, choices=RELEASE_STATUSES, default="PKOM")
     project_duration = models.IntegerField(blank=True)
-    project_is_pm_managed = models.BooleanField(default=False)
+    project_managed_by = models.CharField(max_length=3, default="SQA", choices = MANAGEDBY)
     project_description = models.TextField(max_length=1000, blank=True)
-    project_svn_path = models.CharField(max_length=200, blank=True)
+    project_scm_tool = models.CharField(max_length=3, default="SVN", choices=SCMTOOLS)
+    project_scm_path = models.CharField(max_length=200, blank=True)
     project_soft_path = models.CharField(max_length=200, blank=True)
+    project_bug_tool = models.CharField(max_length=4, default="JIRA", choices = BUGTOOLS)
     project_released_date = models.DateField(blank=True, null=True, help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
+    project_release_status = models.CharField(max_length=10, blank=False, choices=RELEASE_STATUSES, default="PKOM")
 
     def __unicode__(self):
         return self.project_release_name , self.project_uid
@@ -116,6 +130,26 @@ class ProjectReviewDetails(models.Model):
     CHOICES = list((x.choice_code, x.choice_alias)for x in OptionChoice.objects.all())
 
     release_name = models.ForeignKey(ProjectDatabase)
+
+    # PKOM Details
+    pkom_applicable = models.CharField(max_length=3, default="Yes", choices=CHOICES)
+    order_shared = models.BooleanField(default=False)
+    bpw_shared = models.BooleanField(default=False)
+    pkom_checklist_shared = models.BooleanField(default=False)
+    pkom_completed = models.BooleanField(default=False)
+
+    # Project Planning
+    project_plans_applicable = models.CharField(max_length=3, default="Yes", choices=CHOICES)
+    oum_estimator_baselined = models.BooleanField(default = False)
+    pmp_peer_reviewed = models.BooleanField(default=False)
+    pmp_sme_reviewed = models.BooleanField(default=False)
+    pmp_qmg_reviewed = models.BooleanField(default=False)
+    pmp_baselined = models.BooleanField(default=False)
+    mpp_peer_reviewed = models.BooleanField(default=False)
+    mpp_qmg_reviewed = models.BooleanField(default=False)
+    mpp_baselined = models.BooleanField(default=False)
+
+
     # RS related details
     rs_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
     no_of_requirements = models.IntegerField(blank=True)
@@ -123,6 +157,8 @@ class ProjectReviewDetails(models.Model):
 
     # FS related details
     fs_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    fs_panel_review_applicable = models.CharField(choices=CHOICES, max_length=3, default="No")
+    fs_panel_review_mom_shared = models.BooleanField(default=False)
     applicable_fs_documents = models.IntegerField(blank=True)
     fs_signedoff = models.BooleanField(default=False)
     fs_peer_reviewed = models.BooleanField(default=False)
@@ -207,14 +243,21 @@ class ProjectReviewDetails(models.Model):
     itp_qmg_review_done = models.BooleanField(default = False)
     itp_review_comments_closed = models.BooleanField(default=False)
 
+    # STP
+    stp_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    stp_peer_reviewed = models.BooleanField(default=False)
+    stp_dev_reviewed = models.BooleanField(default=False)
+    stp_qmg_reviewed = models.BooleanField(default=False)
+    stp_baselined = models.BooleanField(default=False)
+
     #ITR1 start
     itr1start_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
     itr1_schema_details_obtained = models.BooleanField(default=False)
-    itr1start_tagging_done = models.BooleanField(default=False)
     itr1_code_checkedin = models.BooleanField(default=False)
     itr1_tps_checks_done = models.BooleanField(default=False)
     itr1_fortify_checks_done = models.BooleanField(default=False)
     itr1_start_email_sent = models.BooleanField(default=False)
+    itr1start_tagging_done = models.BooleanField(default=False)
 
     #ITR1 Bugs
     itr1_bug_report_obtained = models.BooleanField(default=False)
@@ -257,6 +300,42 @@ class ProjectReviewDetails(models.Model):
     itr2_bugs_closed = models.BooleanField(default=False)
     itr2_open_bug_details = models.TextField(max_length=200, blank=True)
     itr2_rca_done = models.BooleanField(default=False)
+
+    # Documentation
+    user_manuals_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    help_files_applicable = models.BooleanField(default=False)
+    um_peer_reviewed = models.BooleanField(default=False)
+    um_testteam_reviewed = models.BooleanField(default=False)
+    um_devteam_reviewed = models.BooleanField(default=False)
+    um_qmg_reviewed = models.BooleanField(default=False)
+    um_baselined = models.BooleanField(default=False)
+    help_files_baselined = models.BooleanField(default=False)
+    no_of_user_manuals = models.IntegerField(default = 0)
+
+    # Release
+    release_note_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    release_note_peer_reviewed = models.BooleanField(default=False)
+    release_note_qmg_reviewed = models.BooleanField(default=False)
+    release_note_baselined = models.BooleanField(default=False)
+    dba_report_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    dba_report_peer_reviewed = models.BooleanField(default=False)
+    dba_report_qmg_reviewed = models.BooleanField(default=False)
+    dba_report_baselined = models.BooleanField(default=False)
+    diff_report_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    diff_report_dev_reviewed = models.BooleanField(default=False)
+    scm_report_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    scm_report_dev_reviewed =  models.BooleanField(default=False)
+    scm_report_qmg_reviewed = models.BooleanField(default=False)
+    itr2end_tagging_done = models.BooleanField(default=False)
+
+    # Executables
+    executables_applicable = models.CharField(choices=CHOICES, max_length=3, default="Yes")
+    exec_dev_reviewed = models.BooleanField(default=False)
+    exec_qmg_reviewed = models.BooleanField(default=False)
+    exec_baselined = models.BooleanField(default=False)
+    draftrc_dev_reviewed = models.BooleanField(default=False)
+    finalrc_sent = models.BooleanField(default=False)
+    rc_baselined = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.release_name
